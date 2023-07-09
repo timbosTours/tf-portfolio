dd6b8e9f5628b0d644b26fe87f364604a403a436
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-07-09 22:59:32 +1000

    project page responsive design

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -2,6 +2,18 @@ import { getProject } from "@/sanity/sanity-utils";
 import Image from "next/image";
 import { PortableText } from '@portabletext/react'
 import NavBar from "@/app/components/NavBar";
+import { Antic_Didone } from "next/font/google";
+import { Roboto } from "next/font/google";
+
+const mainFont = Antic_Didone({
+    weight: ['400'],
+    subsets: ['latin']
+})
+
+const roboto = Roboto({
+  weight: ['300', '400'],
+  subsets: ['latin']
+})
 
 type Props = {
     params: {project: string}
@@ -15,12 +27,12 @@ export default async function Project({ params }: Props) {
         <NavBar/>
         <div className="flex flex-col lg:flex-row h-screen pt-20">
             <div className="w-1/2 mx-auto my-auto">
-                <Image src={project.image} alt="project Image" height={540} width={960} className="lg:ml-4"></Image>
+                <Image src={project.image} alt="project Image" height={540} width={960} className="lg:ml-4 mt-10 lg:mt-0"></Image>
             </div>
-            <div className="w-1/2 my-auto mx-auto text-center lg:text-left lg:px-10">
-                <h1 className="">{ project.name }</h1>
-                <div className="my-10"><PortableText value={project.content} /></div>
-                {project.url && <a href={`${project.url}`} title="View Project" target="_blank" rel="noopener noreferrer">
+            <div className="w-1/2 my-auto mx-auto text-center lg:text-left lg:px-10 xl:px-28">
+                <h1 className={`${roboto.className} text-amber-100 text-lg sm:text-3xl md:text-4xl lg:text-5xl` }>{ project.name }</h1>
+                <div className={`${mainFont.className} text-amber-100 my-10` }><PortableText value={project.content} /></div>
+                {project.url && <a href={`${project.url}`} title="View Project" target="_blank" rel="noopener noreferrer" className={`${roboto.className} text-amber-100 underline`}>
                 View Project
             </a>}
             </div>
