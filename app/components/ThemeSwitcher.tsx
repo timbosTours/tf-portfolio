7b50d6e8e0ca8a8c85e999db79fbdc357a249cd9
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-08-13 11:35:48 +1000

    a11y fix

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -26,11 +26,11 @@ export const ThemeSwitcher = () => {
     }
 
     return (
-        <div  aria-hidden="true">
-            <motion.div  aria-hidden="true" whileHover={{ scale: 1.1, transition: { duration: 0.2, type: "spring", stiffness: 400, damping: 10 } }}>
+        <div >
+            <motion.div whileHover={{ scale: 1.1, transition: { duration: 0.2, type: "spring", stiffness: 400, damping: 10 } }}>
             <button  aria-hidden="true" aria-label="switch visual theme" className="dark:hidden mt-1" onClick={toggleTheme}><FaSnowboarding className="fill-amber-100"/></button></motion.div>
             <motion.div  aria-hidden="true" whileHover={{ scale: 1.1, transition: { duration: 0.2, type: "spring", stiffness: 400, damping: 10 } }}>
-            <button  aria-hidden="true" aria-label="switch visual theme" className="hidden dark:inline" onClick={toggleTheme}><FaUserTie /></button></motion.div>
+            <button aria-label="switch visual theme" className="hidden dark:inline" onClick={toggleTheme}><FaUserTie /></button></motion.div>
         </div>
     )
 }
